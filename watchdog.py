a603d68c54520a703c7c4e3cf61f63ed423f3d10
Oulad <moufdi_taha@outlook.fr>
2020-12-29 09:29:27 +0100

    renaming files

--- watchdog.py
+++ watchdog.py
@@ -1,53 +1,25 @@
-from clientThread import ClientTCP
+from watchdogClient import WatchdogClient
 import os
 import threading
 import time
 
-TIMEOUT = 2
-
-class WatchDog(ClientTCP):
+class WatchDog:
     def __init__(self):
-        ClientTCP.__init__(self)
-        self.primary_down = False
-        self.primary_port = 2095
-        self.backup_port = 5094
+        
+        self.client1 = WatchdogClient("primary")
+        self.client2 = WatchdogClient("backup")
         self.primary_server = 'server1'
         self.backup_server = 'server2'
-        self.data = ''
-        self.thread_server1 = threading.Thread(target=self.connectToServer,args=self.primary_port)
-        #self.thread_server2 = threading.Thread(target=self.connectToServer,args=self.backup_port)
-        #self.setTimeout(timeout)
-        self.thread_send = threading.Thread(target=self.send)
-        self.thread_receive = threading.Thread(target=self.receive) 
-        #self.thread_timer = threading.Thread(target=self.start_timer)
-        self.thread_server1.start()
-        self.thread_send.start()
-        self.thread_receive.start()
-          
-        
-        
+        thread_client1 = threading.Thread(target=self.client1.connectToServer,args=[2500])
+        thread_send1 = threading.Thread(target=self.client1.send)
+        thread_receive1 = threading.Thread(target=self.client1.receive) 
+        thread_updatePrimaryServer = threading.Thread(target=self.updatePrimaryServer)
+        thread_client1.start()
+        thread_send1.start()
+        thread_receive1.start()
+        thread_updatePrimaryServer.start()
     
-    #def setTimeout(self,timeout):
-    #    self.timeout = timeout
-
-    def timeout_event(self):
-        print('Oups ! primary server dead')
-        #disconnect from the dead server  
-        print('disconnect from the dead server..')
-        #self.disconnectFromServer()
-       
-        #nominate backup server as primary server and vice versa 
-        self.invertPriority()
-        #self.thread_server2.start()
-        ##turn on the backup server 
-        print('Switching to backup...')
-        print('Turning on backup...')
-        #os.system(self.primary_server+".pys")
-        ##connect to backup 
-        #self.connectToServer()
-        print('Backup now is primary')
-        
-
+    '''
     def deadServer_event(self):
         #disconnect from the dead server  
         self.disconnectFromServer()
@@ -57,43 +29,40 @@ class WatchDog(ClientTCP):
         self.invertPriority()
         #connect to the new primary server (previously backup)
         #self.connectToServer()
-
+    '''
+    
     def invertPriority(self):
         self.primary_server, self.backup_server = self.backup_server, self.primary_server
-
-    def send(self):
-        while True and self.sock != None:
-            message = 'Are you still alive ?'
-            try:
-            # Send data   
-                #print(self.sock._closed)     
-                #print('sending {!r}'.format(message))
-                self.sock.sendall(message.encode())
-                #self.thread_timer.start()
-                self.start_timer()
-                #if self.timeout:
-                #    break
-            except:
-                pass
-
-    def start_timer(self):
-        time.sleep(TIMEOUT)
-        if self.data != 'i am alive':
-            #self.timeout = True
-            self.timeout_event()
-        else:
-            self.data = ''
-        
-    def receive(self):
+   
+    def updatePrimaryServer(self):
         while True:
-            self.data = self.sock.recv(16).decode("utf-8")
-            if(self.data != ''):   
-                print('received {!r}'.format(self.data))
-                #if self.data == 'i am dead':
-                #    self.deadServer_event()
-                #elif self.timeout :
-                #    self.timeout_event()
-        
+            if self.primary_server == "server1" and self.client1.getPriority() == "backup":
+                threads = self.reinit_client2_threads()
+                self.invertPriority()
+                self.client2.setPriority("primary")
+                threads[0].start()
+                threads[1].start()
+                threads[2].start()
+            elif self.primary_server == "server2" and self.client2.getPriority() == "backup":
+                threads = self.reinit_client1_threads()
+                self.invertPriority()
+                self.client1.setPriority("primary")
+                threads[0].start() 
+                threads[1].start() 
+                threads[2].start() 
 
+    def reinit_client1_threads(self):
+        self.client1 = WatchdogClient("primary")
+        thread_client1 = threading.Thread(target=self.client1.connectToServer,args=[2500])
+        thread_send1 = threading.Thread(target=self.client1.send)
+        thread_receive1 = threading.Thread(target=self.client1.receive) 
+        return thread_client1, thread_send1, thread_receive1
+    
+    def reinit_client2_threads(self):
+        self.client2 = WatchdogClient("primary")
+        thread_client2 = threading.Thread(target=self.client2.connectToServer,args=[3003]) 
+        thread_send2 = threading.Thread(target=self.client2.send)
+        thread_receive2 = threading.Thread(target=self.client2.receive) 
+        return thread_client2, thread_send2, thread_receive2
 
 w = WatchDog()
--- watchdogClient.py
+++ watchdogClient.py
@@ -3,44 +3,14 @@ import socket
 import sys
 import time
 import os
+from clientTCP import TCPClient
 
-class ClientTCP :
+TIMEOUT = 2
+
+class WatchdogClient(TCPClient) :
     def __init__(self,priority = "primary"):
-        # Create a TCP/IP socket
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        TCPClient.__init__(self)
         self.priority = priority
-        self._opened = -1 
-        self.port = 0
-        self.data = ''
-
-    def disconnectFromServer(self):
-        #self.sock.shutdown()   
-        self._opened = -1
-        self.sock.close()
-        
-        #os.system('fuser -k '+ str(self.port)+'/tcp')
-    
-    def isDisconnected(self):
-        return self.sock._closed
-    
-    def isConnected(self):
-        return self._opened
-        
-    def connectToServer(self,port):
-        
-        # Connect the socket to the port where the server is listening
-        server_address = ('localhost', port)
-        self.port = port
-        print('connecting to {} port {}'.format(*server_address))
-        self._opened = self.sock.connect_ex(server_address)
-        #print(self.isConnected())
-
-    def receive(self):
-        while True:
-            if self.isConnected() == 0 and self.isDisconnected() == False:
-                self.data = self.sock.recv(16).decode("utf-8")
-                if(self.data != ''):   
-                    print('received {!r} from {}'.format(self.data,self.port))
 
     def send(self):
         while True :
@@ -61,7 +31,7 @@ class ClientTCP :
                     pass
 
     def start_timer(self):
-        time.sleep(2)
+        time.sleep(TIMEOUT)
         if self.data != 'i am alive':
             #self.timeout = True
             self.timeout_event()
